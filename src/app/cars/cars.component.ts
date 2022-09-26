3a2c5949b054158dfe7940e4d475cfa156fd16b1
unlimit3d <i.unlimit3d@gmail.com>
2022-09-26 13:28:53 +0700

    datamodel

--- src/app/cars/cars.component.ts
+++ src/app/cars/cars.component.ts
@@ -1,23 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+
+export interface Car{
+  name:string;
+  model?:string;
+  price:number;
+}
 
 @Component({
   selector: 'app-cars',
   templateUrl: './cars.component.html',
   styleUrls: ['./cars.component.scss']
 })
-export class CarsComponent implements OnInit {
+export class CarsComponent implements OnInit, OnChanges {
 
-  name = 'Honda';
-  model = 'CRV';
-  price = 1000000;
+  // name = 'Honda';
+  // model = 'CRV';
+  // price = 1000000;
 
-  car:any={
+  car:Car={
     name: 'Toyota',
     model: 'VIGO',
-    price: 500000,
+    price: 500000
   };
 
-  cars = [
+  cars:Car[] = [
     {
     name: 'Toyota3',
     model: 'VIGO3',
@@ -27,12 +33,32 @@ export class CarsComponent implements OnInit {
     name: 'Toyota4',
     model: 'VIGO4',
     price: 500004,
+  },{
+    name:'MG',
+    model:'ZZ',
+    price:800000
+  },
+  {
+    name:'Yamaha',
+    price:34000
   }
 ];
 
   constructor() { }
 
   ngOnInit(): void {
+    console.log(this.cars);
+  }
+
+  ngOnChanges(changes: SimpleChanges): void {
+
+  }
+
+  saveCar(){
+    console.log(this.car);
+
+    this.cars.push(this.car);
+
   }
 
 }
