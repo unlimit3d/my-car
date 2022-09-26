9927020d16def8d13e6c3fa7bc7d8eb83dc1d21b
unlimit3d <i.unlimit3d@gmail.com>
2022-09-26 11:55:50 +0700

    style scss

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { NgxSpinnerModule } from "ngx-spinner";
 import { CarsComponent } from './cars/cars.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { CarsComponent } from './cars/cars.component';
     BrowserModule,
     AppRoutingModule,
     NgbModule,
-    NgxSpinnerModule
+    NgxSpinnerModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
