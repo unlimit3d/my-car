68abd6a082972dc0b79e300d81b17b369754e2b1
unlimit3d <i.unlimit3d@gmail.com>
2022-09-27 13:13:43 +0700

    save care

--- src/app/database.service.ts
+++ src/app/database.service.ts
@@ -6,11 +6,29 @@ import { Injectable } from '@angular/core';
 })
 export class DatabaseService {
 
+  urlApi = 'http://localhost';
+
   constructor(private http: HttpClient) { }
 
-// code
-getCars(){
-  return this.http.get('http://localhost/mycar/getCardate.php');
-}
+  // ดึงข้อมูลรถทั้งหมด
+  getCars(){
+    return this.http.get(this.urlApi+'/mycar/getCardate.php');
+  }
+
+  // เพิ่มข้อมูลรถ
+  // car:Car={
+  //   name: '',
+  //   model: '',
+  //   price: 0
+  // };
+  saveCar(car:any){
+    return this.http.post(this.urlApi+'/mycar/pushCardate.php',car);
+  }
+
+  // update ข้อมูลรถ
+
+  // ลบข้อมูลรถ
+
+  // ข้อมูลรถ id
 
 }
